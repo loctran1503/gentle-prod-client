b3d59a3c59a3562560cf886923a6af15fdfaf1c6
Loc Tran <loc@example.com>
2022-05-07 15:29:45 +0700

    change servername

--- src/utils/lib/ApolloClient.ts
+++ src/utils/lib/ApolloClient.ts
@@ -11,7 +11,7 @@ import JwtManager from "../other/JwtManager";
 const httpLink = new HttpLink({
   uri:
     process.env.NODE_ENV === "production"
-      ? "http://137.184.230.138"
+      ? "https://gentle-server-1.uk"
       : "http://localhost:4000/graphql",
   credentials: "include",
 });
